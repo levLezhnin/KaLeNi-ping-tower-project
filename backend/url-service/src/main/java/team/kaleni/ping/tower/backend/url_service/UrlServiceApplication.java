a0483717f59e0a23d9c65b0f8846c4160d9fae5b
Nexonm <nikita96max@gmail.com>
2025-09-07 15:35:52 +0300

    Initialize the project

---
+++ backend/url-service/src/main/java/team/kaleni/ping/tower/backend/url_service/UrlServiceApplication.java
@@ -0,0 +1,13 @@
+package team.kaleni.ping.tower.backend.url_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlServiceApplication.class, args);
+	}
+
+}
