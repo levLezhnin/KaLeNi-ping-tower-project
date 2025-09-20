79b3ca5aa284d8c41b7be89eec4e36fece90a593
Nexonm <nikita96max@gmail.com>
2025-09-20 13:13:50 +0300

    initiate project

---
+++ backend/ping-service/src/main/java/team/kaleni/ping/tower/backen/ping_service/PingServiceApplication.java
@@ -0,0 +1,13 @@
+package team.kaleni.ping.tower.backen.ping_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingServiceApplication.class, args);
+	}
+
+}
