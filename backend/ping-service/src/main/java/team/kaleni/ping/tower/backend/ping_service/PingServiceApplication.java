80465bf43a23535de91e17840ebf253f720bfbb7
levLezhn <lezhnin.lev78@gmail.com>
2025-09-21 17:48:58 +0500

    README changes

--- backend/ping-service/src/main/java/team/kaleni/ping/tower/backend/ping_service/PingServiceApplication.java
+++ backend/ping-service/src/main/java/team/kaleni/ping/tower/backend/ping_service/PingServiceApplication.java
@@ -2,11 +2,8 @@ package team.kaleni.ping.tower.backend.ping_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import team.kaleni.ping.tower.backend.ping_service.dto.NotificationDTO;
-import team.kaleni.ping.tower.backend.ping_service.producer.NotificationProducer;
 
 @SpringBootApplication
 @EnableAsync
@@ -14,9 +11,7 @@ import team.kaleni.ping.tower.backend.ping_service.producer.NotificationProducer
 public class PingServiceApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(PingServiceApplication.class, args);
-		NotificationProducer notificationProducer = context.getBean(NotificationProducer.class);
-		notificationProducer.sendNotification(new NotificationDTO("Test", 1L));
+		SpringApplication.run(PingServiceApplication.class, args);
 	}
 
 }
